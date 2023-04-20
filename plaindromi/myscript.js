173f3ded161ad417c21815cc5cb05170a87c40e6
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-20 15:31:26 +0200

    add console.log("la tua parola è palindroma")

--- plaindromi/myscript.js
+++ plaindromi/myscript.js
@@ -5,6 +5,28 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 // Inserire un prompt per chiedere all'utente di inserire una parola
 
-let parolaUtente = prompt("Inserisci una parola")
+let parolaUtente = prompt("Inserisci una parola");
+console.log("La parola inserita è:" + " " + parolaUtente);
 
-// Creare una funzione che verifichi se la parola è palindroma oppure no
+// scomponiamo la parola in lettera per lettera
+
+let parolaDivisa = parolaUtente.split('');
+console.log(parolaDivisa);
+
+// scriviamo la parola al contrario
+
+let parolaInvertita = parolaDivisa.reverse('');
+console.log(parolaInvertita);
+
+// riuniamo la parola scritta al contario
+
+let parolaAlContrario = parolaInvertita.join('');
+console.log(parolaAlContrario);
+
+// confrontiamo le due parole
+
+if (parolaUtente == parolaAlContrario){
+    console.log("La tua parola è palindroma");
+} else{
+    console.log("La tua parola non è palindroma");
+}
