45e71a52b169b6f1cf0d4c93fca271942e5a9da3
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-20 15:11:07 +0200

    add prompt in "palindromi"

---
+++ plaindromi/myscript.js
@@ -0,0 +1,10 @@
+/*
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// Inserire un prompt per chiedere all'utente di inserire una parola
+
+let parolaUtente = prompt("Inserisci una parola")
+
+// Creare una funzione che verifichi se la parola è palindroma oppure no
